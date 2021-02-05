41f2cb5458deb7e9631fd41baedfc29014caf88f
LeonardoGracioS <samuelgracio@gmail.com>
2021-02-05 19:55:36 +0100

    add figures

--- covidex/plot/main.py
+++ covidex/plot/main.py
@@ -12,5 +12,6 @@ def plot_death_per_date(dates, death_data, place):
            title='Évolution du nombre de décès (Hors Ehpad) à ' +str(place)+ ' en fonction du temps')
     plt.xticks(rotation='vertical')
     ax.grid()
+    plt.savefig("../figures/death_"+str(place)+'_'+str(dates[0])+'_'+str(dates[-1])+'.jpeg')
 
     plt.show()
